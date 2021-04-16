7222956b84b7d94e2e12bce91e136f397d844c46
Mateusz <mateusz.milewski5.stud@pw.edu.pl>
2021-04-16 03:57:48 +0200

    removed apollo

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,26 +2,18 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import AppNavigator from './AppNavigator';
-
-const client = new ApolloClient({
-  uri: 'https://graphql-api-dog-breeds.herokuapp.com/graphql',
-  cache: new InMemoryCache(),
-});
 
 export default () => {
   return (
     <SafeAreaProvider>
-      <ApolloProvider client={client}>
-        <NavigationContainer>
-          <StatusBar
-            //   backgroundColor={Colors.WHITE_COLOR}
-            barStyle={'dark-content'}
-          />
-          <AppNavigator />
-        </NavigationContainer>
-      </ApolloProvider>
+      <NavigationContainer>
+        <StatusBar
+          //   backgroundColor={Colors.WHITE_COLOR}
+          barStyle={'dark-content'}
+        />
+        <AppNavigator />
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 };
